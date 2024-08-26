f523a3e6fbfc1cb45d31d85fbee573b1c7d7b43f
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2024-08-26 14:50:53 -0400

    handling routes

--- basics/app.js
+++ basics/app.js
@@ -17,12 +17,22 @@ const express = require('express');
 //const server = http.createServer(routes.handler);
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('in the middleware choro');
-  next(); // allows the request to continue to the next middleware in line
+// app.use((req, res, next) => {
+//   console.log('in the middleware choro');
+//   next(); // allows the request to continue to the next middleware in line
+// });
+
+app.use('/', (req, res, next) => {
+  console.log('This always run');
+  next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+  console.log('in another middleware choro');
+  res.send('<h1>The "Add Product" Page</h1>');
+});
+
+app.use('/', (req, res, next) => {
   console.log('in another middleware choro');
   res.send('<h1>Hello from express!</h1>');
 });
