c24c9a4798ff998364340385d87820b7dc3ab292
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2024-09-02 12:08:18 -0400

    serving html pages

--- basics/routes/admin.js
+++ basics/routes/admin.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
@@ -5,9 +6,10 @@ const router = express.Router();
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   // console.log('in another middleware choro');
-  res.send(
-    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>'
-  );
+  // res.send(
+  //   '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>'
+  // );
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
