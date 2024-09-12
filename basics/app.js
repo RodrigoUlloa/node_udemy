d0002b24fc25bf088d6c8846e2071f39307f2bd7
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2024-09-12 19:04:27 -0300

    converting html files to pug

--- basics/app.js
+++ basics/app.js
@@ -46,7 +46,8 @@ app.use(shopRoutes);
 
 app.use((req, res, next) =>{
   // res.status(404).send('<h1>Page not found</h1>');
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404');
 });
 
 app.listen(3000);
--- basics/routes/admin.js
+++ basics/routes/admin.js
@@ -12,7 +12,8 @@ router.get('/add-product', (req, res, next) => {
   // res.send(
   //   '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>'
   // );
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {pageTitle: 'Add Product'});
 });
 
 // /admin/add-product => POST
