8793d41ebf6f0eb53f7b0fc569ee0ffe31b9cd29
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2024-09-13 18:01:51 -0300

    working with ejs

--- basics/app.js
+++ basics/app.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+
+// const expressHbs = require('express-handlebars');
+
 //const routes = require('./routes');
 // function rqListener(req, res){
 
@@ -19,15 +21,17 @@ const expressHbs = require('express-handlebars');
 //const server = http.createServer(routes.handler);
 const app = express();
 
-app.engine(
-  'hbs',
-  expressHbs({
-    layoutsDir: 'views/layout/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs'
-  })
-);
-app.set('view engine', 'hbs');
+// app.engine(
+//   'hbs',
+//   expressHbs({
+//     layoutsDir: 'views/layout/',
+//     defaultLayout: 'main-layout',
+//     extname: 'hbs'
+//   })
+// );
+
+
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
