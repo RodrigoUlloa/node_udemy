581f7b04c7cf4b40a3aee84906d207cb33a6b820
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2024-09-22 20:56:10 -0300

    storing data file and fetching datgqa file via model

--- basics/controllers/products.js
+++ basics/controllers/products.js
@@ -13,19 +13,20 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   //products.push({ title: req.body.title });
-  const product = new Product.new(req.body.title);
+  const product = new Product(req.body.title);
   product.save();
   res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll();
-  res.render('shop', {
-    prods: products, 
-    pageTitle: 'Shop', 
-    path: '/',
-    hasProducts: products.lenght > 0,
-    activeShop: true,
-    productCSS:true
+  Product.fetchAll(products => {
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
   });
 };
