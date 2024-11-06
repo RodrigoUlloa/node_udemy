7962234ef6b7b53e23d696b35c31b19988b26850
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2024-11-06 14:50:35 -0300

    usign postgresql, creating database.js, conecting and basic sql, retrieving data

--- basics/app.js
+++ basics/app.js
@@ -20,7 +20,7 @@ const bodyParser = require('body-parser');
 
 //const server = http.createServer(routes.handler);
 const app = express();
-
+const db = require('./util/database');
 // app.engine(
 //   'hbs',
 //   expressHbs({
@@ -29,6 +29,15 @@ const app = express();
 //     extname: 'hbs'
 //   })
 // );
+
+// testing in bd
+// db.query('SELECT * FROM products')
+//   .then(result => {
+//     console.log(result.rows, result.fields);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
 
 const errorController = require('./controllers/error');
 
