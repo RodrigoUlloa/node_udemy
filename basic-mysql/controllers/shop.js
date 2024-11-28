1d4177341a44cea90bc70d0841a6d3746949cd91
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2024-11-28 00:10:12 -0300

    reset cart, fetching and outputting orders

--- basic-mysql/controllers/shop.js
+++ basic-mysql/controllers/shop.js
@@ -113,16 +113,18 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user
     .getCart()
     .then(cart => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then(products => {
-      req.user
+      return req.user
       .createOrder()
       .then(order => {
-        order.addProducts(
+        return order.addProducts(
           products.map(product => {
             product.orderItem = { quantity: product.cartItem.quantity };
             return product;
@@ -132,21 +134,30 @@ exports.postOrder = (req, res, next) => {
       .catch(err => console.log(err));
     })
     .then(result => {
+      return fetchedCart.setProducts(null);
+    })
+    .then(result => {
       res.redirect('/orders');
     })
     .catch(err => console.log(err));
-}
+};
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
+  req.user
+    .getOrders({include: ['products']})
+    .then(orders => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders || []
+      });
+    })
+    .catch(err => console.log(err));
 };
 
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout'
-  });
-};
+// exports.getCheckout = (req, res, next) => {
+//   res.render('shop/checkout', {
+//     path: '/checkout',
+//     pageTitle: 'Checkout'
+//   });
+// };
