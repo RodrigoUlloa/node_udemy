e0fdc53590ff00d6a1b16b9682b68d558e064295
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2025-01-31 20:15:36 -0300

    usign db conection

--- basic-mysql/app.js
+++ basic-mysql/app.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database');
+const mongoConnect = require('./util/database').mongoConnect;
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -21,6 +21,7 @@ app.use((req, res, next) => {
   //     next();
   //   })
   //   .catch(err => { console.log(err)});
+  next();
 });
 
 app.use('/admin', adminRoutes);
--- basic-mysql/models/product.js
+++ basic-mysql/models/product.js
@@ -8,30 +8,16 @@ class Product {
     this.imageUrl = imageUrl;
   }
   save() {
-
+    const db = getDb();
+    db.collection('products')
+    .insertOne(this)
+    .then(result => {
+      console.log(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
   }
 }
-
-const Product = sequilize.define('product', {
-  id: {
-    type: Sequilize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequilize.STRING,
-  price: {
-    type: Sequilize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequilize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequilize.STRING,
-    allowNull: false
-  }
-});
 
 module.exports = Product;
