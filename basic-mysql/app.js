edfafe0d922c90998f05cf250214ac3eee11f5f6
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2025-05-14 23:07:54 -0400

    creating login form, request driven login, setting manipulating config a cockie

--- basic-mysql/app.js
+++ basic-mysql/app.js
@@ -5,12 +5,14 @@ const mongoose = require('mongoose');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 const app = express();
+const MONGO_DB = process.env;
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -26,11 +28,12 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes)
 
 app.use(errorController.get404);
 
 mongoose
-  .connect('mongodb+srv://rodrigo:nPrKktz3NJiLVMLG@cluster0.vxqev.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0')
+  .connect(MONGO_DB)
   .then(result =>{
     User.findOne().then(user => {
       if (!user) {
