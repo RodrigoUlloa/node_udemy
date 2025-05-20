ec2a039086d8b315b3b322b7a55b9dc866ef2654
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2025-05-19 20:51:12 -0400

    signin funcionality, route protection partialy

--- basic-mysql/controllers/auth.js
+++ basic-mysql/controllers/auth.js
@@ -18,14 +18,31 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById("682502eabb3fd03f1aeaecec")
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({email: email})
     .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save((err) => {
-        console.log(err);
-        res.redirect('/');
-      });
+      if (!user) {
+        return res.redirect('login')
+      }
+      bycrpt
+        .compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch){
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save((err) => {
+              console.log(err);
+              res.redirect('/');
+            });
+          }
+          res.redirect('/login');
+        })
+        .catch(err => {
+          console.log(err);
+          res.redirect('/login');
+        });
+      
     })
     .catch(err => { console.log(err)});
 };
