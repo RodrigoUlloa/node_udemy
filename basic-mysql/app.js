848be7efd90dcb9108c8a8907401d709122a589c
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2025-05-19 20:14:50 -0400

    implementing authentication flow

--- basic-mysql/app.js
+++ basic-mysql/app.js
@@ -58,18 +58,6 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then(result =>{
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Piri',
-          email: 'piri@test.cl',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    })
     app.listen(3000);
   })
   .catch(err => {
