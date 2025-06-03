86656ca0959a7f3ebe60d3d00ea6616f1b998205
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2025-06-03 19:19:30 -0400

    implementing token logic

--- basic-mysql/controllers/auth.js
+++ basic-mysql/controllers/auth.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const bycrpt = require('bcryptjs');
 const User = require('../models/user');
 const sendMail = require("@sendgrid/mail");
@@ -122,3 +123,38 @@ exports.getReset = (req, res, next) => {
     errorMessage: message
   });
 };
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect('/reset');
+    }
+    const token = buffer.toString('hex');
+    User.findOne({ email: req.body.emil })
+    .then(user => {
+      if(!user) {
+        req.flash('error', 'No account with taht email found.');
+        return res.redirect('/reset');
+      }
+      user.resetToken = token;
+      user.resetTokenExpiration = Date.now() + 3600000;
+      return user.save();
+    })
+    .then(result => {
+      res.redirect('/');
+      sendMail.send({
+        to: req.body.email,
+        from: "rodrigo.ulloa@proton.me",
+        subject: "Password reset",
+        text: "You successfully",
+        html: `
+          <p> You requested a password reset </p>
+          <p> Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password </p>
+        `
+      })
+    .catch(err => {
+      console.log(err);
+    })
+  });
+}};
