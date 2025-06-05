6bdc34ae1b94ed4be8b8caaa7a7e7e85abbe54cb
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2025-06-05 14:27:41 -0400

    more validatiors

--- basic-mysql/routes/auth.js
+++ basic-mysql/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator');
+const { check, body } = require('express-validator');
 const authController = require('../controllers/auth');
 const router = express.Router();
 
@@ -11,16 +11,25 @@ router.post('/login', authController.postLogin);
 
 router.post(
   '/signup', 
-  check('email')
+  [
+    check('email')
     .isEmail()
     .withMessage('Please enter a valid email')
     .custom((value, {req}) => {
       if (value === 'test@test.com'){
         throw new Error('This email addess if forbidden.');
       }
-      return true
-    }), 
-  authController.postSignup);
+      return true;
+    }),
+    body(
+      'password', 
+      'Please enter a password with only numbers and text and at least 5 characters.'
+    )
+      .isLength({min: 5})
+      .isAlphanumeric()
+  ], 
+  authController.postSignup
+);
 
 router.post('/logout', authController.postLogout);
 
