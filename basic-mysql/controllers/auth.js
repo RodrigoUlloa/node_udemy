da2dc6a190b2d5f77aa207c89e06943423e09e68
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2025-06-10 22:22:29 -0400

    keeping user input

--- basic-mysql/controllers/auth.js
+++ basic-mysql/controllers/auth.js
@@ -30,7 +30,12 @@ exports.getSignup = (req, res, next) => {
   res.render('auth/signup',{
     path: '/signup',
     pageTitle: 'Signup',
-    errorMessage: message
+    errorMessage: message,
+    oldInput: {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
   });
 };
 
@@ -40,7 +45,7 @@ exports.postLogin = (req, res, next) => {
 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.render('auth/login',{
+    return res.status(422).render('auth/login',{
       path: '/login',
       pageTitle: 'Login',
       errorMessage: errors.array()[0].msg
@@ -79,13 +84,19 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
 
+  const errors = validationResult(req);
   if(!errors.isEmpty()){
     console.log(errors.array());
     return res.status(422).render('auth/signup',{
       path: '/signup',
       pageTitle: 'Signup',
-      errorMessage: errors.array()[0].msg
-    });;
+      errorMessage: errors.array()[0].msg,
+      oldInput: { 
+        email: email, 
+        password: password, 
+        confirmPassword: req.body.confirmPassword 
+      }
+    });
   }
   
   bycrpt
