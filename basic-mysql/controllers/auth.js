318cb8134a48c3a952f047911be89db13409e062
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2025-06-11 22:44:04 -0400

    adding validation to login

--- basic-mysql/controllers/auth.js
+++ basic-mysql/controllers/auth.js
@@ -16,7 +16,12 @@ exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    errorMessage: message
+    errorMessage: message,
+    oldInput: {
+      email: '',
+      password: ''
+    },
+    validationErrors: []
   });
 };
 
@@ -49,15 +54,28 @@ exports.postLogin = (req, res, next) => {
     return res.status(422).render('auth/login',{
       path: '/login',
       pageTitle: 'Login',
-      errorMessage: errors.array()[0].msg
+      errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password
+      },
+      validationErrors: errors.array()
     });
   }
 
   User.findOne({email: email})
     .then(user => {
       if (!user) {
-        req.flash('error', 'Invalid email or password.');
-        return res.redirect('/login')
+        return res.status(422).render('auth/login', {
+          path: '/login',
+          pageTitle: 'Login',
+          errorMessage: 'Invalid email or password.',
+          oldInput: {
+            email: email,
+            password: password
+          },
+          validationErrors: []
+        });
       }
       bycrpt
         .compare(password, user.password)
@@ -70,7 +88,16 @@ exports.postLogin = (req, res, next) => {
               res.redirect('/');
             });
           }
-          res.redirect('/login');
+          return res.status(422).render('auth/login', {
+            path: '/login',
+            pageTitle: 'Login',
+            errorMessage: 'Invalid email or password.',
+            oldInput: {
+              email: email,
+              password: password
+            },
+            validationErrors: []
+          });
         })
         .catch(err => {
           console.log(err);
@@ -113,17 +140,20 @@ exports.postSignup = (req, res, next) => {
   })
   .then(result => {
     res.redirect('/login');
-    return sendMail.send({
-      to: email,
-      from: "rodrigo.ulloa@proton.me",
-      subject: "Signup succeeded! ",
-      text: "You successfully",
-      html: "<h1>You successfully signed up baby!</h1>",
-    })
-    .then(() => {
-      console.log("Email sent");
-    })
+    // return sendMail.send({
+    //   to: email,
+    //   from: "rodrigo.ulloa@proton.me",
+    //   subject: "Signup succeeded! ",
+    //   text: "You successfully",
+    //   html: "<h1>You successfully signed up baby!</h1>",
+    // })
+    // .then(() => {
+    //   console.log("Email sent");
+    // })
   })
+  .catch(err => {
+    console.log(err);
+  });
 };
 
 exports.postLogout = (req, res, next) => {
