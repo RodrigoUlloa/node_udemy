970bc01313410ad24947da66dc6095a40950fd97
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2025-06-19 13:45:04 -0400

    Downloading files with authenticaction

--- basic-mysql/controllers/shop.js
+++ basic-mysql/controllers/shop.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const Product = require('../models/product');
 const Order = require('../models/order');
 
@@ -147,7 +150,18 @@ exports.getOrders = (req, res, next) => {
   });
 };
 
-// exports.getCheckout = (req, res, next) => {
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+  const invoiceName= 'invoice-' + orderId + '.pdf';
+  const invoicePath = path.join('data', 'invoices', invoiceName);
+  fs.readFile(invoicePath, (err, data) => {
+      if (err) {
+        next(err);
+      }
+      res.setHeader('Content-Type', 'application/pdf');
+      res.send(data);
+  });
+};
 //   res.render('shop/checkout', {
 //     path: '/checkout',
 //     pageTitle: 'Checkout'
