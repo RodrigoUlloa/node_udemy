af0afd03dec56a0781ebbfbbc9b327899a97aecc
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2025-06-19 14:12:26 -0400

    streaming data vs preloading

--- basic-mysql/controllers/shop.js
+++ basic-mysql/controllers/shop.js
@@ -152,7 +152,7 @@ exports.getOrders = (req, res, next) => {
 
 exports.getInvoice = (req, res, next) => {
   const orderId = req.params.orderId;
-  const invoiceName= 'invoice-' + orderId + '.pdf';
+  
   Order.findById(orderId).then(order => {
     if (!order) {
       return next(new Error('Order not found'));
@@ -160,16 +160,22 @@ exports.getInvoice = (req, res, next) => {
     if (order.user.userId.toString() !== req.user._id.toString()) {
       return next(new Error('Unauthorized access'));
     }
+    const invoiceName= 'invoice-' + orderId + '.pdf';
     const invoicePath = path.join('data', 'invoices', invoiceName);
-    fs.readFile(invoicePath, (err, data) => {
-      if (err) {
-        return next(err);
-      }
-      res.setHeader('Content-Type', 'application/pdf');
-      res.setHeader('Content-Disposition', 'inline; filename="invoice-' + orderId + '"');
-      res.send(data);
-    });
-  }).catch(err => next(err));
+    // fs.readFile(invoicePath, (err, data) => {
+    //   if (err) {
+    //     return next(err);
+    //   }
+    //   res.setHeader('Content-Type', 'application/pdf');
+    //   res.setHeader('Content-Disposition', 'inline; filename="invoice-' + orderId + '"');
+    //   res.send(data);
+    // });
+    const file = fs.createReadStream(invoicePath);
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'inline; filename="invoice-' + orderId + '"');
+    file.pipe(res);
+  })
+  .catch(err => next(err));
 };
 //   res.render('shop/checkout', {
 //     path: '/checkout',
