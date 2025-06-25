55b47967fbea0ceb3382c077a90dd87a97cc3f9d
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2025-06-25 01:06:27 -0400

    sending post

--- rest-api/controllers/feed.js
+++ rest-api/controllers/feed.js
@@ -7,6 +7,7 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
+  
   res.status(201).json({
     message: 'Post created successfully!',
     post:{ id: new Date().toISOString(), title: title, content: content }
