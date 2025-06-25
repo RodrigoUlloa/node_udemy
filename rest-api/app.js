62af1e4ab9543c6035b7220437f6b934ba95e8a1
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2025-06-25 18:21:07 -0400

    seting up post model

--- rest-api/app.js
+++ rest-api/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
+
+const {MONGODB_URI} = process.env;
 
 const app = express();
 
@@ -16,4 +19,11 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose
+  .connect(MONGODB_URI)
+  .then(result =>{
+    app.listen(8080);
+  })
+  .catch(err => {
+    console.log(err);
+  });
