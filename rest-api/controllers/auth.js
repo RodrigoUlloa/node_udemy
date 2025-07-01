779628d3b5281a1935dfe8f26b244af7db80c5cc
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2025-07-01 01:17:55 -0400

    logging JWTs

--- rest-api/controllers/auth.js
+++ rest-api/controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -40,7 +41,7 @@ exports.login = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   let loadedUser;
-  User.findOne({emai: email})
+  User.findOne({email: email})
     .then(user => {
       if(!user){
         const error = new Error('A user with this email could not be found');
@@ -48,15 +49,23 @@ exports.login = (req, res, next) => {
         throw error;
       }
       loadedUser = user;
-      return bcrypt.comapare(password, user.password);
+      return bcrypt.compare(password, user.password);
     })
-    then(isEqual => {
+    .then(isEqual => {
       if(!isEqual){
         const error = new Error('Wrong password!');
         error.statusCode = 401;
         throw error;
       }
-      
+      const token = jwt.sign(
+        {
+          email: loadedUser.email, 
+          userId: loadedUser._id.toString()
+        }, 
+        'somesupersecretsecret',
+        { expiresIn: '1h'}
+      );
+      res.status(200).json({token: token, userId: loadedUser._id.toString() });
     })
     .catch(err => {
       if (!err.statusCode) {
