2e18335a66e5516bb98915b0235c74fc2138a138
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2025-07-03 00:34:24 -0400

    that sichroizing  POST but not the code have old conventions

--- rest-api/controllers/feed.js
+++ rest-api/controllers/feed.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const {validationResult} = require('express-validator');
 
+const io = require('../socket');
 const Post = require('../models/post');
 const User = require('../models/user');
 
@@ -56,11 +57,12 @@ exports.createPost = async (req, res, next) => {
     const user = await User.findById(req.userId);
       user.posts.push(post);
     await user.save();
-      res.status(201).json({
-        message: 'Post created successfully',
-        post: post,
-        creator: { _id: user._id, name: user.name }
-      });
+    io.getIO().emit('posts', {action: 'create', post: post});
+    res.status(201).json({
+      message: 'Post created successfully',
+      post: post,
+      creator: { _id: user._id, name: user.name }
+    });
   } catch (err) {
     if (!err.statusCode){
       err.statusCode = 500;
