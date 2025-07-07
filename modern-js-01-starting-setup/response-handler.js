e987df38592f29ec81e114b1526fd1e545b77330
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2025-07-07 14:09:51 -0400

    node core modules & promises

--- modern-js-01-starting-setup/response-handler.js
+++ modern-js-01-starting-setup/response-handler.js
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import fs from 'fs/promises';
 import path, {dirname} from 'path';
 import { fileURLToPath } from 'url';
 
@@ -6,13 +6,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export const resHandler = (req, res, next) => {
-  // fs.readFile('my-page.html', 'utf8', (err, data) => {
-  //   if (err) {
-  //     return next(err);
-  //   }
-  //   res.send(data);
-  // });
-  res.sendFile(path.join(__dirname, 'my-page.html'));
+  fs.readFile('my-page.html', 'utf8')
+    .then((data) => {
+      res.send(data);
+    }).catch((err) => {
+      next(err);
+    });
+  // res.sendFile(path.join(__dirname, 'my-page.html'));
 };
 
 //module.exports = resHandler;
