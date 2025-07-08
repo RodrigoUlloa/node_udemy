a105e5cd2b2b1a74c4f613262fa9a94cc5647f9a
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2025-07-08 12:42:04 -0400

    using type casting, but the delete route dont work

--- typescript-app/routes/todos.ts
+++ typescript-app/routes/todos.ts
@@ -2,6 +2,9 @@ import { NextFunction, Router } from 'express';
 
 import { Todo } from '../models/todo';
 
+type RequestBody = { text: string };
+type RequestParams = {todoId: string }
+
 let todos: Todo[] = [];
 
 const router = Router();
@@ -11,9 +14,10 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/todo', (req, res, next) => {
+  const body = req.body as RequestBody;
   const newTodo: Todo = {
     id: new Date().toISOString(),
-    text: req.body.text,
+    text: body.text,
   };
 
   todos.push(newTodo);
@@ -21,20 +25,23 @@ router.post('/todo', (req, res, next) => {
 });
 
 router.put('/todo/:todoId', (req, res, next) => {
-  const tid = req.params.todoId;
+  const params = req.params as RequestParams;
+  const tid = params.todoId;
+  const body = req.body as RequestBody;
   const todoIndex = todos.findIndex((todoItem) => todoItem.id === tid);
   if (todoIndex >= 0){
-    todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+    todos[todoIndex] = { id: todos[todoIndex].id, text: body.text };
     res.status(200).json({ message: 'Update todo', todos: todos }); 
     return;
   }
   res.status(404).json({ message: 'Could not find todo for this id.'})
 });
 
-router.delete('todo/todoId', (req , res, next) => {
-  todos = todos.filter(todoItem => todoItem.id !== (req.params as { todoId:string }).todoId);
+//this route dont work well
+router.delete('todo/todoId', (req, res, next) => {
+  const params = req.params as RequestParams;
+  todos = todos.filter(todoItem => todoItem.id !== (params.todoId));
   res.status(200).json({ message: 'Deleted todo', todos: todos });
- 
 })
 
 export default router;
